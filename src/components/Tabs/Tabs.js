84dd7a427b2831c82ba9f694bae067c25da1f380
MattyMags <mattmag18@gmail.com>
2023-10-27 13:39:51 -0700

    enh: move color toggle to header

---
+++ src/components/Tabs/Tabs.js
@@ -0,0 +1,57 @@
+import { Tabs } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
+
+import { IconTilde, IconBriefcase, IconMathGreater, IconBulb } from '@tabler/icons-react';
+
+// import Intro from '../Intro/Intro';
+// import SkillSet from '../Skillset/Skillset';
+
+const TabsList = () => {
+  const mediumScreen = useMediaQuery('(min-width: 48em)');
+  return (
+    <Tabs
+      defaultValue="introduction"
+      color="indigo"
+      orientation={mediumScreen ? 'vertical' : 'horizontal'}>
+      <Tabs.List>
+        <Tabs.Tab value="introduction" icon={mediumScreen ? <IconTilde size="0.8rem" /> : null}>
+          Home
+        </Tabs.Tab>
+        <Tabs.Tab
+          value="workExperience"
+          icon={mediumScreen ? <IconBriefcase size="0.8rem" /> : null}>
+          {mediumScreen ? 'Work Experience' : 'Experience'}
+        </Tabs.Tab>
+        <Tabs.Tab
+          value="technicalExperience"
+          icon={mediumScreen ? <IconMathGreater size="0.8rem" /> : null}>
+          {mediumScreen ? 'Techinal Skillset' : 'Skillset'}
+        </Tabs.Tab>
+        <Tabs.Tab value="interests" icon={mediumScreen ? <IconBulb size="0.8rem" /> : null}>
+          {mediumScreen ? 'Interests for 2023' : 'Interests'}
+        </Tabs.Tab>
+      </Tabs.List>
+
+      <Tabs.Panel value="introduction" pt="xs">
+        {/* <Intro /> */}
+      </Tabs.Panel>
+
+      <Tabs.Panel value="workExperience" pt="xs">
+        Messages tab content
+      </Tabs.Panel>
+
+      <Tabs.Panel value="technicalExperience" pt="xs">
+        {/* <SkillSet
+          title="Developing with Purpose: A Spotlight on My Core Development Skills"
+          description=""
+        /> */}
+      </Tabs.Panel>
+
+      <Tabs.Panel value="interests" pt="xs">
+        Settings tab content
+      </Tabs.Panel>
+    </Tabs>
+  );
+};
+
+export default TabsList;
