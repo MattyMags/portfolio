1be746aa205866557dafbca7ab74cd8695f3bba0
MattyMags <mattmag18@gmail.com>
2023-10-27 12:20:35 -0700

    feat: add logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,10 @@ import cx from 'clsx';
 import classes from '@components/Header/Header.module.css';
 
 import { Container } from '@mantine/core';
+import Logo from '@components/Logo/Logo';
 
 const Header = () => {
-  return <Container className={cx(classes.container)}>Header</Container>;
+  return <Container className={cx(classes.container)}><Logo/></Container>;
 };
 
 export default Header;
